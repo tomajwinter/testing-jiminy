a817d715e58178d16602eb68830eaa3926689458
Tom Winter <tom.winter@meetcleo.com>
2023-05-02 14:34:25 +0100

    Setup Users -> Orders n+1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :orders
+
+  def self.all_order_stuff
+    all.map do |user|
+      user.orders.map { |order| order.description }
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   test "creates a user with a first name" do
     assert User.create(first_name: 'Nope')
   end
+
+  test ".all_order_stuff" do
+    user = User.create
+    user_two = User.create
+    [user, user_two].each do |user|
+      5.times { |i| Order.create(user: user, description: "An order for #{i} stuff") }
+    end
+
+    User.all_order_stuff
+  end
 end
